151b8020ea02dd4f9cd5decd97cf143f03f6fdc5
MyMac <nagornyykostya@gmail.com>
2020-01-24 14:28:16 +0200

    hw1

--- lesson2/hw1/index.js
+++ lesson2/hw1/index.js
@@ -3,9 +3,9 @@ let b = 2;
 
 console.log('a: ', a);
 console.log('b: ', b);
-console.log('a - b: ', a - b);
-console.log('a + b: ', a + b);
-console.log('a / b: ', a / b);
-console.log('a * b: ', a * b);
+console.log('a - b = ', a - b);
+console.log('a + b = ', a + b);
+console.log('a / b = ', a / b);
+console.log('a * b = ', a * b);
 
 
