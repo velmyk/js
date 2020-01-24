7471b1ca64283a4161321a5665bde12e832bcf74
MyMac <nagornyykostya@gmail.com>
2020-01-24 16:04:44 +0200

    task1

--- lesson3/task1/index.js
+++ lesson3/task1/index.js
@@ -1,50 +1,50 @@
 console.log(`+17: `, +17);
-console.log(`+'77':`, +'77');
-console.log(`10 + '5':`, 10 + '5');
-console.log(`'some' + 'text':`, 'some' + 'text');
-console.log(`undefined + 1:`, undefined + 1);
-console.log(`null + 8:`, null + 8);
-console.log(`undefined + '5':`, undefined + '5');
-console.log(`7 - null:`, 7 - null);
-console.log(`5 - undefined:`, 5 - undefined);
-console.log(`'text' + null:`, 'text' + null);
+console.log(`+'77': `, +'77');
+console.log(`10 + '5': `, 10 + '5');
+console.log(`'some' + 'text': `, 'some' + 'text');
+console.log(`undefined + 1: `, undefined + 1);
+console.log(`null + 8: `, null + 8);
+console.log(`undefined + '5': `, undefined + '5');
+console.log(`7 - null: `, 7 - null);
+console.log(`5 - undefined: `, 5 - undefined);
+console.log(`'text' + null: `, 'text' + null);
 
 let num = 17;
 
-console.log(`++num:`, ++num);
-console.log(`num++:`, num++);
+console.log(`++num: `, ++num);
+console.log(`num++: `, num++);
 
 const a = 17;
 
-console.log(`a = 17:`, a);
+console.log(`a = 17: `, a);
 
 
 const b = -a;
 
-console.log(`b = -a:`, -a);
+console.log(`b = -a: `, -a);
 
 
 
-console.log(`'24' / 4:`, '24' / 4);
-console.log(`18 * '3':`, 18 * '3');
+console.log(`'24' / 4: `, '24' / 4);
+console.log(`18 * '3': `, 18 * '3');
 
-console.log(`'9' - '3':`, '9' - '3');
+console.log(`'9' - '3': `, '9' - '3');
 
-console.log(`'seventeen' / 3:`, 'seventeen' / 3);
+console.log(`'seventeen' / 3: `, 'seventeen' / 3);
 
-console.log(`4 * 'four':`, 4 * 'four');
+console.log(`4 * 'four': `, 4 * 'four');
 
-console.log(`17 / 0:`, 17 / 0);
+console.log(`17 / 0: `, 17 / 0);
 
-console.log(`10 + 'seven':`, 10 + 'seven');
+console.log(`10 + 'seven': `, 10 + 'seven');
 
-console.log(`undefined + 1:`, undefined + 1);
+console.log(`undefined + 1: `, undefined + 1);
 
-console.log(`null + 8:`, null + 8);
+console.log(`null + 8: `, null + 8);
 
-console.log(`undefined + '5':`, undefined + '5');
+console.log(`undefined + '5': `, undefined + '5');
 
-console.log(`'text' + null:`, 'text' + null);
+console.log(`'text' + null: `, 'text' + null);
 
 
 
