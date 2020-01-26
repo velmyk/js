32504368200133d9da2a41e01d95315000e7118a
MyMac <nagornyykostya@gmail.com>
2020-01-26 18:58:14 +0200

    hw5

--- lesson5/hw5/index.js
+++ lesson5/hw5/index.js
@@ -1,8 +1,8 @@
 function findDivCount(a, b, n) {
     result = 0;
     for (let i = a; i <= b; i++) {
-        if (n % i == 0) {
-            result++;
+        if (i % n == 0) {
+            result ++;
         }
     }
     return result;
