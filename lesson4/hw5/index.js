686f3333a3eb561a3fd3e206c2def2fc7ad4fd32
MyMac <nagornyykostya@gmail.com>
2020-01-26 08:19:37 +0200

    hw5

--- lesson4/hw5/index.js
+++ lesson4/hw5/index.js
@@ -1,7 +1,8 @@
 let sum = 0;
+const divider = 1234;
 
 for (let i = 1; i <= 1000; i++) {
     sum += i;
 }
 
-console.log(Math.floor(sum / 1234) > (sum % 1234));
+console.log(Math.floor(sum / divider) > (sum % divider));
