8e43660566b4fde03cfab38db1510d28ed37f101
MyMac <nagornyykostya@gmail.com>
2020-01-26 16:06:02 +0200

    deleted isInteger function

--- lesson4/hw2/index.js
+++ lesson4/hw2/index.js
@@ -3,35 +3,35 @@ let n = 30;
 
 let result = 0;
 
-function isInteger(num) {
-    return (num ^ 0) === num;
+// function isInteger(num) {
+//     return (num ^ 0) === num;
+// }
+
+for (let i = m; i <= n; i++) {
+    if ((i % 2 == 0) && (i % 4 != 0)) {
+        result += i;
+    } else if (i % 3 == 0) {
+        result -= i;
+    } else if (i % 4 == 0) {
+        result *= i;
+    } else if (i % 5 == 0) {
+        console.log(i);
+    }
 }
+console.log(result);
+
 
 // for (let i = m; i <= n; i++) {
-//     if (isInteger(i / 2)  && isInteger(i / 4)){
+
+//     if (isInteger(i / 5)) {
+//         console.log(i);
+//        continue;
+//     } else if (isInteger(i / 2) && !isInteger(i / 4)) {
 //         result += i;
 //     } else if (isInteger(i / 3)) {
 //         result -= i;
 //     } else if (isInteger(i / 4)) {
 //         result *= i;
-//     } else if (isInteger(i / 5)) {
-//         console.log(i);
 //     }
 // }
 // console.log(result);
-
-
-for (let i = m; i <= n; i++) {
-
-    if (isInteger(i / 5)) {
-        console.log(i);
-       continue;
-    } else if (isInteger(i / 2) && !isInteger(i / 4)) {
-        result += i;
-    } else if (isInteger(i / 3)) {
-        result -= i;
-    } else if (isInteger(i / 4)) {
-        result *= i;
-    }
-}
-console.log(result);
