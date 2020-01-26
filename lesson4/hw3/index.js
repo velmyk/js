8453a43fac91b6bdd2120a766d8bc4ec441a6438
MyMac <nagornyykostya@gmail.com>
2020-01-26 08:00:16 +0200

    hw3

---
+++ lesson4/hw3/index.js
@@ -0,0 +1,12 @@
+let sum = 0;
+
+for (let i = 0; i <= 1000; i++) {
+    if (i % 2 != 0) {
+        console.log("Found");
+        sum += i;
+    } else if ( (sum * 5) > 5000) {
+        console.log("Bigger");
+    } else {
+        console.log("Smaller or equal")
+    }
+}
