2031d9d2378418da18105968602d211cb9642e77
MyMac <nagornyykostya@gmail.com>
2020-01-26 07:43:27 +0200

    hw2

--- lesson4/hw2/index.js
+++ lesson4/hw2/index.js
@@ -1,21 +1,37 @@
-let m = 5;
-let n = 15;
+let m = 7;
+let n = 30;
 
 let result = 0;
 
 function isInteger(num) {
     return (num ^ 0) === num;
-  }
+}
+
+// for (let i = m; i <= n; i++) {
+//     if (isInteger(i / 2)  && isInteger(i / 4)){
+//         result += i;
+//     } else if (isInteger(i / 3)) {
+//         result -= i;
+//     } else if (isInteger(i / 4)) {
+//         result *= i;
+//     } else if (isInteger(i / 5)) {
+//         console.log(i);
+//     }
+// }
+// console.log(result);
+
 
 for (let i = m; i <= n; i++) {
-    if (isInteger(i / 2)  && isInteger(i / 4)){
+
+    if (isInteger(i / 5)) {
+        console.log(i);
+        continue;
+    } else if (isInteger(i / 2) && isInteger(i / 4)) {
         result += i;
     } else if (isInteger(i / 3)) {
         result -= i;
     } else if (isInteger(i / 4)) {
         result *= i;
-    } else if (isInteger(i / 5)) {
-        console.log(i);
     }
 }
 console.log(result);
