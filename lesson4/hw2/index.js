35317dddc845b890f22c218a7e61110bd9e5652a
MyMac <nagornyykostya@gmail.com>
2020-01-26 07:27:49 +0200

    hw 2

---
+++ lesson4/hw2/index.js
@@ -0,0 +1,22 @@
+let m = 5;
+let n = 15;
+
+let result = 0;
+
+function isInteger(num) {
+    return (num ^ 0) === num;
+  }
+
+for (let i = 0; i <= n; i++) {
+    if (isInteger(i / 2)  && isInteger(i / 4)){
+        result += i;
+    } else if (isInteger(i / 3)) {
+        result -= i;
+    } else if (isInteger(i / 4)) {
+        result *= i;
+    } else if (isInteger(i / 5)) {
+        console.log(i);
+    }
+}
+console.log(result);
+
