80f7312ddf02d01648ac42d517bba8528776b287
MyMac <nagornyykostya@gmail.com>
2020-01-28 09:19:16 +0200

    hw2

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -1,9 +1,13 @@
 const getRandomNumbers = (length, start, finish) => {
 
     let result = [];
-    
-    for (let i = 0; i <= length; i++) {
-        result.push(Math.random() * (start - finish) + finish);
+
+    if (start >= finish) {
+        for (let i = 0; i <= length - 1; i++) {
+            result.push(Math.random() * (start - finish) + finish);
+        }
+        return result;
+    } else {
+        return null;
     }
-    return result;
 }
