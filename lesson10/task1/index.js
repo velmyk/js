5b9aa18f472e235c00896318f9c750be26cc74c4
MyMac <nagornyykostya@gmail.com>
2020-01-28 07:51:07 +0200

    task1

---
+++ lesson10/task1/index.js
@@ -0,0 +1,18 @@
+const getFiniteNumbers = arr =>
+    arr.filter(item => Number.isFinite(item));
+
+const getFiniteNumbersV2 = arr =>
+    arr.filter(item => isFinite(item));
+
+const getNaN = arr =>
+    arr.filter(item => Number.isNaN(item));
+
+const getNaNV2 = arr =>
+    arr.filter(item => isNaN(item));
+
+const getIntegers = arr =>
+    arr.filter(item => !Number.isInteger(item));
+
+// let arr1 = [2, 3, 5, Infinity, 5];
+
+// console.log(getFiniteNumbers(arr1))
