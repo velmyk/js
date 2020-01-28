faa3241c1d837dd8c8ff3c5e7eea0753a707d018
MyMac <nagornyykostya@gmail.com>
2020-01-28 05:43:20 +0200

    hw2

--- lesson9/hw2/index.js
+++ lesson9/hw2/index.js
@@ -1,30 +1,30 @@
 "use strict";
 
-function getPeople(obj) {
-    // let result = Object.entries(obj);
-    //     result.flat();
-    // let {...rest} = obj;
-    // let {...other} = rest.name;
-
-    return result;
-}
+const getPeople = obj =>
+    Object.entries(obj)
+        .map(item => item[1])
+        .flat()
+        .map(item => item.name)
 
 
-const rooms = {
-    room1:[
-        { name: 'room1 name1'},
-        { name: 'room1 name2'},
-        { name: 'room1 name3'},
-        { name: 'room1 name4'},
-    ], 
-    room2:[
-        { name: 'room2 name1'}
-    ],
-    room3:[
-        { name: 'room3 name1'},
-        { name: 'room3 name2'},
-        { name: 'room3 name3'},
-    ]
-}
 
-console.log(getPeople(rooms));
+
+
+// const rooms = {
+//     room1: [
+//         { name: 'room1 name1' },
+//         { name: 'room1 name2' },
+//         { name: 'room1 name3' },
+//         { name: 'room1 name4' },
+//     ],
+//     room2: [
+//         { name: 'room2 name1' }
+//     ],
+//     room3: [
+//         { name: 'room3 name1' },
+//         { name: 'room3 name2' },
+//         { name: 'room3 name3' },
+//     ]
+// }
+
+// console.log(getPeople(rooms));
